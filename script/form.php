1c50b5da3c8b49ba89ec99862a553c33a61e6bda
NASDAMI Quatadah <Quatadah.Nasdami@bordeaux-inp.fr>
2021-12-01 02:29:52 +0100

    including form to contact

--- script/form.php
+++ script/form.php
@@ -1,20 +1,113 @@
-<html>
-<body>
 
 <?php
+
+$requiredfield = "";
+$nameErr = $emailErr = "";
+$name = $email = $subject = $message = "";
+function test_input($data){
+    $data = htmlspecialchars($data);
+    $data = trim($data);
+    $data = stripslashes($data);
+    return $data;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = test_input($_POST["name"]);
+    if (empty($name)){
+        $nameErr = "Name is required";
+        $requiredfield = "* Requiered field";
+    }
+    $email = test_input($_POST["email"]);
+    if (empty($email)){
+        $emailErr = "Email is required";
+        $requiredfield = "* Requiered field";
+    }
+    $subject = test_input($_POST["subject"]);
+    $message = test_input($_POST["message"]);
+}
+
+
+/*
+
+$name = $email = $subject = $message = "";
+$nameErr = $emailErr = "";
+
+function test_input($data){
+    $data = htmlspecialchars($data);
+    $data = trim($data);
+    $data = stripslashes($data);
+    return $data;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = test_input($_POST["name"]);
+    if (empty($name)){
+        $nameErr = "Name is required";
+    }
+    $email = test_input($_POST["email"]);
+    if (empty($email)){
+        $emailErr = "Email is required";
+    }
+    $subject = test_input($_POST["subject"]);
+    $message = test_input($_POST["message"]);
+
+}
+
+echo "<h1> Your input </h1>";
+echo $nameErr . "<br>";
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 $name = $email = $subject = $message = "";
 $nameErr = $emailErr = $subjectErr = $messageErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     if (empty($_POST["name"])){
         $nameErr = "Name is required";
+        echo $nameErr;
     } else{
         if (!preg_match("/[a-zA-Z-' ]*$/", $name)){
             $nameErr = "Only letters and white spaces are allowed";
         }
         else{
-            $name = test_input($_POST["name"]);
-            
+            $name = test_input($_POST["name"]);         
         }
     }
     if (empty($_POST["email"])){
@@ -48,12 +141,8 @@ function test_input($data){
 
 
 echo "<h2>Your input :</h2>";
+echo "Name :" . $name;
 echo "<br>";
-echo "Name : $name";
-echo "<br>";
-echo "Email: $email";
-
+echo "Email:" . $email;
+*/
 ?>
-Hi <?php $name ?> your email adress is <?php $email ?>
-</body>
-</html>
